f63fb615a094a7d92f6c47b78466661133eb7a57
Vaibhavrai1804 <dhruv2005rai@gmail.com>
2026-07-31 19:17:10 +0530

    Simple Calculator Program

---
+++ Calculator.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+
+    //Taking Input from the user
+    cout<<"Enter First Number :";
+    cin>>a;
+    cout<<"Enter Second Number :";
+    cin>>b;
+
+    //Perform Operation
+
+    cout<<"Addition :"<<a+b<<endl;
+    cout<<"Subtraction :"<<a-b<<endl;
+    cout<<"Multiplication :"<<a*b<<endl;
+    cout<<"Division :"<<a/b<<endl;
+
+
+
+// --------------OUTPUT---------------
+/*
+Enter First Number :10
+Enter Second Number :4
+Addition :14
+Subtraction :6
+Multiplication :40
+Division :2
+*/
+
+
+
+}
