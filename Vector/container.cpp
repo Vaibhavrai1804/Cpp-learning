704ae8b77353c34295ba975d6e7c260b9a370c14
Vaibhavrai1804 <dhruv2005rai@gmail.com>
2026-08-23 17:05:23 +0530

    Added Container With Most Water solution using brute force approach (O(n²) time complexity)

--- Vector/container.cpp
+++ Vector/container.cpp
@@ -5,7 +5,7 @@ int maxarea(vector<int>& height){
     int n=height.size();
     int max_area=0;
     for(int i=0;i<n;i++){
-        int width=0;
+        int width=1;
         for(int j=i+1;j<n;j++){
             int length=min(height[i],height[j]);
             int area=length*width;
@@ -16,7 +16,7 @@ int maxarea(vector<int>& height){
     return max_area;
 }
 int main(){
-    vector<int>height={0,2};
+    vector<int>height={1,8,6,2,5,4,8,3,7};
     int ans=maxarea(height);
     cout<<"Maximum water a conatiner can contain :"<< ans;
 }
